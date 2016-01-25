8a9746ce65b375bd5df69bced7a9716923bad4fd
gnetscher <gnetscher@gmail.com>
2016-01-24 16:48:10 -0800

    - finished ImDataDir

--- demo.py
+++ demo.py
@@ -3,7 +3,21 @@ import chain as ch
 from os import path as osp
 
 def run_rcnn():
-	imPath = '/mnt/HardDrive/data/try/outimg_Angle1Lighting1'	
-	imName = osp.join(imPath, '0.jpg')
-	imProd = ch.ImDataFile()
-	return imProd
+    imPath = '/mnt/HardDrive/datatry/outimg_Angle1Lighting1'
+    imName = osp.join(imPath, '0.jpg')
+    imProd = ch.ImDataFile()
+    return imProd
+
+def run_test():
+    imPath = 'try/'
+    # imName = osp.join(imPath, '0.jpg')
+    # imProd = ch.ImDataFile()
+
+    imProd = ch.ImDataDir()
+    iter_ = imProd.produce(imPath)
+    for it in iter_:
+        print '~~~~~~ IMAGE ~~~~~~'
+        print it
+
+if __name__ == '__main__':
+    run_test()
