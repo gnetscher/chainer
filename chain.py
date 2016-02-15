45b5e4cfeb1f65e5343c14783a8cfaf0661fa93b
Pulkit <pulkitag@berkeley.edu>
2016-02-15 11:22:41 -0800

    Generalizing chainer from linear to feedforward chains

--- chain.py
+++ chain.py
@@ -43,7 +43,38 @@ class Chainer(object):
 		class that strings together chains
 	'''
 	def __init__(self, objList=[]):
-		self.chainObjs_ = objList
+		self.chainObjs_ = []
+		N  = len(objList)
+		#ip2opIdx[i] is a list that stores to what modules the o/p
+		#of ith module must be sent
+		self.ip2opIdx_ = []
+		#ips[i] is a list that stores inputs to ith module
+		self.ips_      = []
+		for n in range(N):
+			self.ip2opIdx_.append([])
+			self.ips_.append([])
+		#Organize what input needs to be fed into what o/p
+		for n,o in enumerate(objList):
+			if type(o) == tuple:
+				if len(o) == 1:
+					obj  = o[0]
+					conn = ((-1,0))
+				else:
+					assert len(o) == 2, 'Format not recognized'
+					self.chainObjs_.append(o[0])
+					obj, conn = o
+					assert type(conn) == tuple, 'Connectivity must be tuple'	
+			if n == 0:
+				continue
+			for cn in conn:
+				modNum, ipNum = cn
+				if modNum == -1:
+					self.ip2opIdx_[n-1].append((n, ipNum))
+				else:
+					assert modNum < n, 'Modules can only be chained in\
+                feedforward chains'
+					self.ip2opIdx_[modNum].append((n, ipNum)) 	
+				
 
 	def produce(self, ip=None):
 		ip = copy.deepcopy(ip)
